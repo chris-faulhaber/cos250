8d4f1e4e52025cf8da62e866fa230727116be825
Chris Faulhaber <cfaulhaber@cashstar.com>
2015-01-03 14:37:09 -0500

    eb configs

--- nand2tetris/settings.py
+++ nand2tetris/settings.py
@@ -53,13 +53,14 @@ ROOT_URLCONF = 'nand2tetris.urls'
 WSGI_APPLICATION = 'nand2tetris.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
 
